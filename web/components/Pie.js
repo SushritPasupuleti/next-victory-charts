38e8aa35a5541aaad96df6b51502eca152dfffba
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2021-07-24 18:42:25 +0530

    add gradient effect to chart

--- web/components/Pie.js
+++ web/components/Pie.js
@@ -11,12 +11,47 @@ const data = [
 export default function Pie() {
     return (
         <div>
+            <svg style={{visibility: 'hidden'}}>
+                <defs>
+                    <linearGradient id="gradient1"
+                        x1="0%" y1="0%" x2="0%" y2="100%"
+                    >
+                        <stop offset="0%" stopColor="orange" />
+                        <stop offset="100%" stopColor="red" />
+                    </linearGradient>
+                    <linearGradient id="gradient2"
+                        x1="0%" y1="0%" x2="0%" y2="100%"
+                    >
+                        <stop offset="0%" stopColor="yellow" />
+                        <stop offset="100%" stopColor="orange" />
+                    </linearGradient>
+                    <linearGradient id="gradient3"
+                        x1="0%" y1="0%" x2="0%" y2="100%"
+                    >
+                        <stop offset="0%" stopColor="yellow" />
+                        <stop offset="100%" stopColor="yellowGreen" />
+                    </linearGradient>
+                    <linearGradient id="gradient4"
+                        x1="0%" y1="0%" x2="0%" y2="100%"
+                    >
+                        <stop offset="0%" stopColor="yellowGreen" />
+                        <stop offset="100%" stopColor="green" />
+                    </linearGradient>
+                </defs>
+            </svg>
             <V.VictoryPie
-            //   colorScale={["tomato", "orange", "gold", "cyan", "navy" ]}
+                //   colorScale={["tomato", "orange", "gold", "cyan", "navy" ]}
                 data={data}
                 theme={V.VictoryTheme.material}
-                animate={{duration: 500}}
+                animate={{ duration: 500 }}
                 responsive={true}
+                // labels={(datum) => {datum.y / 125}}
+                colorScale={[
+                    "url(#gradient1)",
+                    "url(#gradient2)",
+                    "url(#gradient3)",
+                    "url(#gradient4)",
+                ]}
             />
         </div>
     )
